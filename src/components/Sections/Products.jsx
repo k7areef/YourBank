2ee59aa070aa5215e1974311f7e2b36c992e53b1
k7areef <kh3reef@gmail.com>
2026-03-18 05:32:26 +0200

    feat: add icons

--- src/components/Sections/Products.jsx
+++ src/components/Sections/Products.jsx
@@ -1,14 +1,41 @@
 import SectionHeader from "./common/SectionHeader";
+import data from "@data/products.json";
+import workBagIcon from "@assets/icons/work-bag.png";
+import saveIcon from "@assets/icons/save.png";
+import monyIcon from "@assets/icons/money.png";
+
+const iconsMaper = {
+    "work-bag": workBagIcon,
+    "save": saveIcon,
+    "mony": monyIcon
+}
 
 function Products() {
+    const { title, wordsColored, description, products } = data;
     return (
         <section className="products-section py-5 md:py-10" id="products">
             <div className="container">
                 <SectionHeader
-                    title="Our Products"
-                    wordsColored={["Products"]}
-                    description="Discover a range of comprehensive and customizable banking products at YourBank, designed to suit your unique financial needs and aspirations"
+                    title={title}
+                    wordsColored={wordsColored}
+                    description={description}
                 />
+                <div className="cards-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                    {
+                        products.map((pro, index) => (<div className="product-card text-center p-5 not-first-of-type:border-s not-first-of-type:border-grey-15" key={pro.id || index}>
+                            <div className="icon-wrapper mb-3 w-23 h-23 mx-auto flex items-center justify-center rounded-full bg-linear-to-b from-green-60/5 to-green-60/0">
+                                <div className="icon-container w-18 h-18 flex items-center justify-center rounded-full bg-linear-to-b from-green-60/10 via-green-60/0 via-47% to-transparent">
+                                    <img
+                                        src={iconsMaper[pro.icon]}
+                                        alt={pro.title}
+                                    />
+                                </div>
+                            </div>
+                            <h2 className="mb-2 text-lg sm:textxl md:text-2xl">{pro.title}</h2>
+                            <p>{pro.description}</p>
+                        </div>))
+                    }
+                </div>
             </div>
         </section>
     )
--- src/components/Sections/UseCases.jsx
+++ src/components/Sections/UseCases.jsx
@@ -1,8 +1,35 @@
+import SectionHeader from "./common/SectionHeader";
+import data from "@data/useCases.json";
+import handMoneyIcon from "@assets/icons/hand-money.png";
+import saveMoneyIcon from "@assets/icons/save-money.png";
+import bankIcon from "@assets/icons/bank.png";
+import openBookMoneyIcon from "@assets/icons/open-book.png";
+import companyIcon from "@assets/icons/company.png";
+import moneyIcon from "@assets/icons/money.png";
+import analyticsIcon from "@assets/icons/analytics.png";
+import moneyCoinIcon from "@assets/icons/money-coin.png";
+
+const iconsMaper = {
+    "money-hand": handMoneyIcon,
+    "save-money": saveMoneyIcon,
+    "bank": bankIcon,
+    "book-open": openBookMoneyIcon,
+    "company": companyIcon,
+    "money": moneyIcon,
+    "analytics": analyticsIcon,
+    "money-coin": moneyCoinIcon
+}
+
 function UseCases() {
+    const { title, wordsColored, description } = data;
     return (
         <section className="use-cases-section" id="useCases">
             <div className="container">
-                Use Cases
+                <SectionHeader
+                    title={title}
+                    wordsColored={wordsColored}
+                    description={description}
+                />
             </div>
         </section>
     )
